ecae1b03b8aef240b7af08a40c912a86fd2aefda
Ognjen Stanić <o.stanic@vegaitsourcing.rs>
2020-08-25 17:15:18 +0200

    Add pipline configuration for publishing.

---
+++ devops/scripts/build_release.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+export BINARY=terraform-provider-squidex
+
+cd `dirname "$0"`/../../
+
+echo "Create artifacts dir."
+mkdir -p artifacts
+
+echo "Building provider for darwin..."
+GOOS=darwin GOARCH=amd64 go build -o ./artifacts/${BINARY}_${VERSION}_darwin_amd64
+echo "Done."
+echo "Building provider for linux..."
+GOOS=linux GOARCH=amd64 go build -o ./artifacts/${BINARY}_${VERSION}_linux_amd64
+echo "Done."
+echo "Building provider for windows..."
+GOOS=windows GOARCH=amd64 go build -o ./artifacts/${BINARY}_${VERSION}_windows_amd64
+echo "Done."
+
+echo "Zipping provider..."
+
+cd artifacts
+
+zip ${BINARY}_${VERSION}_darwin_amd64.zip ./${BINARY}_${VERSION}_darwin_amd64
+zip ${BINARY}_${VERSION}_linux_amd64.zip ./${BINARY}_${VERSION}_linux_amd64
+zip ${BINARY}_${VERSION}_windows_amd64.zip ./${BINARY}_${VERSION}_windows_amd64
+
+rm ./${BINARY}_${VERSION}_darwin_amd64
+rm ./${BINARY}_${VERSION}_linux_amd64
+rm ./${BINARY}_${VERSION}_windows_amd64
+
+cd ../../
+echo "Done."
---
+++ devops/scripts/check_deps.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+sudo apt install -y golang
+sudo apt install -y zip
