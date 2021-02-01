530ff77467808cc4ca1c83eed0a721b5b54a0f51
Michiel van Klinken <michiel@qvision.nl>
2021-02-01 15:14:35 +0100

    fix checksum

--- devops/scripts/build_release.sh
+++ devops/scripts/build_release.sh
@@ -11,15 +11,21 @@ mkdir -p artifacts
 echo "Done."
 
 echo "Step 3. Building provider for linux..."
-GOOS=linux GOARCH=amd64 go build -o ./linux/terraform-provider-squidex_v${VERSION}
+GOOS=linux GOARCH=amd64 go build -o ./artifacts/linux/terraform-provider-squidex_v${VERSION}
 echo "Done."
 
-echo "Step 4. Building provider for windows..."
-GOOS=windows GOARCH=amd64 go build -o ./windows/terraform-provider-squidex_v${VERSION}.exe
+echo "Step 3. Building provider for windows..."
+GOOS=windows GOARCH=amd64 go build -o ./artifacts/windows/terraform-provider-squidex_v${VERSION}.exe
+echo "Done."
+
+echo "Step 4. Changing current dir to $(pwd)/artifacts"
+cd ./artifacts
 echo "Done."
 
 echo "Step 5. Zipping provider..."
-zip ./artifacts/terraform-provider-squidex_v${VERSION}_linux_amd64.zip ./linux/terraform-provider-squidex_v${VERSION}
-zip ./artifacts/terraform-provider-squidex_v${VERSION}_windows_amd64.zip ./windows/terraform-provider-squidex_v${VERSION}.exe
+zip terraform-provider-squidex_v${VERSION}_linux_amd64.zip ./linux/terraform-provider-squidex_v${VERSION}
+zip terraform-provider-squidex_v${VERSION}_windows_amd64.zip ./windows/terraform-provider-squidex_v${VERSION}.exe
+rm ./linux/terraform-provider-squidex_v${VERSION}
+rm ./windows/terraform-provider-squidex_v${VERSION}.exe
 sha256sum -b * >SHA256SUMS
 echo "Done."
