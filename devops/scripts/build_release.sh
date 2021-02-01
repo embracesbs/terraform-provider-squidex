c9d4432a106211b713fc8b76765c54854497584a
Michiel van Klinken <michiel@qvision.nl>
2021-02-01 15:07:43 +0100

    test package multiple binaries

--- devops/scripts/build_release.sh
+++ devops/scripts/build_release.sh
@@ -11,15 +11,15 @@ mkdir -p artifacts
 echo "Done."
 
 echo "Step 3. Building provider for linux..."
-GOOS=linux GOARCH=amd64 go build -o ./artifacts/terraform-provider-squidex_v${VERSION}
+GOOS=linux GOARCH=amd64 go build -o ./linux/terraform-provider-squidex_v${VERSION}
 echo "Done."
 
-echo "Step 4. Changing current dir to $(pwd)/artifacts"
-cd ./artifacts
+echo "Step 4. Building provider for windows..."
+GOOS=windows GOARCH=amd64 go build -o ./windows/terraform-provider-squidex_v${VERSION}.exe
 echo "Done."
 
 echo "Step 5. Zipping provider..."
-zip terraform-provider-squidex_v${VERSION}_linux_amd64.zip ./terraform-provider-squidex_v${VERSION}
-rm ./terraform-provider-squidex_v${VERSION}
+zip ./artifacts/terraform-provider-squidex_v${VERSION}_linux_amd64.zip ./linux/terraform-provider-squidex_v${VERSION}
+zip ./artifacts/terraform-provider-squidex_v${VERSION}_windows_amd64.zip ./windows/terraform-provider-squidex_v${VERSION}.exe
 sha256sum -b * >SHA256SUMS
 echo "Done."
